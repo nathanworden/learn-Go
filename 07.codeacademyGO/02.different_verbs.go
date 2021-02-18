8822b6452c33d5930cd805d2fb422a26f72fefa1
Nathan Worden <worden.nathan@gmail.com>
2021-02-18 12:10:55 -0800

    update go notes, add different_verbs

---
+++ 07.codeacademyGO/02.different_verbs.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	votingAge := 18
+	fmt.Printf("You must be %d years old to vote.", votingAge)
+
+	gpa := 3.8
+	fmt.Printf("You're averaging: %.2f.", gpa)
+}
