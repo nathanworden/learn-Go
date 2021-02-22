091ad00da7af42bec9bea08f6d9095ece4910756
Nathan Worden <worden.nathan@gmail.com>
2021-02-22 09:55:48 -0800

    learning go

---
+++ 08.tour.golang.org/20.slices.go
@@ -0,0 +1,26 @@
+// Slices
+
+// An array has a fized size. A slice, on the other hand, is a dynamically-sized, flexible view into the eleemnts of an array. In practice, slices are much more common than arrays.
+
+// The type []T is a slice with elements of type T.
+
+//  A slice is formed by specifiying two indicies, a low and high bound, separated by a colon:
+
+// a[low : high]
+
+// This selects a half-open range which includes the first elementm but excludes the last one.
+
+// The following expression creates a slice which includes elements 1 through 3 of a:
+
+// a[1:4]
+
+package main
+
+import "fmt"
+
+func main() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+}
