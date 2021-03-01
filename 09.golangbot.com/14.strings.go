7dde0f94fb6868938b4dca0f9561a57a36aa66ef
Nathan Worden <worden.nathan@gmail.com>
2021-03-01 15:37:29 -0800

    add strings

---
+++ 09.golangbot.com/14.strings.go
@@ -0,0 +1,15 @@
+// Strings
+
+// What is a String?
+// A string is a slice of bytes in Go. Strings can be created by enclosing a set of characters inside double quotes ""
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name := "Hello World"
+	fmt.Println(name)
+}
